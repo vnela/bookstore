5db6bea91a313f9b0d716c4661a8b90bb765e966
unknown <venla.vihersaari@gmail.com>
2019-09-09 18:41:41 +0300

    bookform

--- bookstore/src/main/java/hh/swd20/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,11 +24,33 @@ public class BookController {
 		return "index";
 	}
 
+	// kirjalistaus
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
 	public String getBooks(Model model) {
-			List<Book> books =  (List<Book>) bookRepository.findAll(); 
-			model.addAttribute("books", books); 
-			return "booklist"; 
+		List<Book> books = (List<Book>) bookRepository.findAll();
+		model.addAttribute("books", books);
+		return "booklist";
 	}
 
+	// tyhjän kirjalomakkeen muodostaminen
+	@RequestMapping(value = "/newbook", method = RequestMethod.GET)
+	public String getNewBookForm(Model model) {
+		model.addAttribute("book", new Book());
+		return "bookform";
+	}
+
+	// kirjalomakkeella syötettyjen tietojen vastaanotto ja tallennus
+	@RequestMapping(value = "/newbook", method = RequestMethod.POST)
+	public String saveBook(@ModelAttribute Book book) {
+
+		bookRepository.save(book);
+		return "redirect:/booklist";
+	}
+
+	// kirjan poisto
+	@RequestMapping(value = "/deletebook/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
 }
