e3dae486c35efcdb8c7e218f8bb07e93416884fe
unknown <venla.vihersaari@gmail.com>
2019-09-09 13:49:20 +0300

    booklist page lisätty

--- bookstore/src/main/java/hh/swd20/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -1,10 +1,14 @@
 package hh.swd20.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
 
 @Controller
@@ -12,10 +16,17 @@ import hh.swd20.bookstore.domain.BookRepository;
 public class BookController {
 	@Autowired
 	BookRepository bookRepository;
-	
+
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String getHello() {
 		return "index";
 	}
-	
+
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String getBooks(Model model) {
+			List<Book> books =  (List<Book>) bookRepository.findAll(); 
+			model.addAttribute("books", books); 
+			return "booklist"; 
+	}
+
 }
